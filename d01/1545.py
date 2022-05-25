17d56363c6a2802c23ff4d4178e6b4d314ba92bb
dolpongg <96533697+dolpongg@users.noreply.github.com>
2022-05-25 21:08:32 +0900

    220525

---
+++ d01/1545.py
@@ -0,0 +1,3 @@
+num_list = [str(i) for i in range(int(input()), -1, -1)]
+
+print(" ".join(num_list))
---
+++ d01/2063.py
@@ -0,0 +1,4 @@
+lang = int(input())
+num_list = sorted(list(map(int, input().split())))
+
+print(num_list[lang//2])
